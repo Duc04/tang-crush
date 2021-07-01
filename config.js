22dcbe8cf8d9038dc1342d0d26b61d5c781e3440
Duc04 <86769658+Duc04@users.noreply.github.com>
2021-07-01 15:21:10 +0700

    Duc04:patch-1
    oki

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Babi à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    desc: 'Cho anh thêm 1 cơ hội để quan tâm và iu thương em được hok ',
+    btnYes: '1 lần nữa thôi đấy <33',
+    btnNo: 'Không,mơ đi :3',
+    question:'anh hok hứa nhưng anh sẽ cố gắng hết sức làm em vuii <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: ' <33333333',
+    mess: 'Anh biết màa. Ỏ dth z troii 😘😘',
+    messDesc: 'Từ nay mỗi tối anh sẽ đều voice chúc em ngủ ngon nhaa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
